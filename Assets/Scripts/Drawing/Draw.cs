18a167cb0feca8d0645dcac14387c096d8ba4ac4
Atahan Ekici <atahanekici@hotmail.com>
2023-03-30 12:25:40 +0300

    Error Message added.
    Added a message when a colliding object found in the spawnned object perimeter.

--- Assets/Scripts/Drawing/Draw.cs
+++ Assets/Scripts/Drawing/Draw.cs
@@ -1,9 +1,8 @@
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 public class Draw : MonoBehaviour
 {
     private const string LineMaterialResourcePath = "Materials/LineMaterial/LineMaterial";
+    private const string DrawPhysicsMaterialResourcePath = "Materials/LineMaterial/DrawMaterial";
 
     [Header("Line Renderer")]
     [SerializeField] private LineRenderer Line_Renderer;
@@ -21,15 +20,14 @@ public class Draw : MonoBehaviour
     [SerializeField] private Camera MainCamera;
 
     [Header("Simplification Coefficient")]
-    [SerializeField] private float SimplificationCoefficient = 0.01f;
+    [SerializeField] private float SimplificationCoefficient = 0.05f;
 
     [Header("Line Material")]
     [SerializeField] private Material LineMaterial;
+    [SerializeField] private PhysicsMaterial2D physicMaterial2D;
     private void Awake()
     {
-        LineMaterial = GetLineMaterial();
-        Drawings = new("Drawings");
-        Drawings.AddComponent<DrawingContainer>();
+        StartUp();
     }
     private void OnEnable()
     {
@@ -40,13 +38,11 @@ public class Draw : MonoBehaviour
         GetMousePosition();
         GetMouseInputs();
     }
-    void OnMouseEnter()
+    private void StartUp()
     {
-        Debug.Log("Mouse entered the gameobject");
-    }
-    void OnMouseExit()
-    {
-        Debug.Log("Mouse exited the gameobject");
+        GetResources();
+        Drawings = new("Drawings");
+        Drawings.AddComponent<DrawingContainer>();
     }
     private void GetMouseInputs()
     {
@@ -76,8 +72,6 @@ public class Draw : MonoBehaviour
     }
     private void StartDrawing()
     {
-        //if (HasAnyObjectOnMouse()) { DisposeLineRenderer(); return; }
-
         NewDrawing = new("Drawing"+ TotalCount.ToString());
 
         Line_Renderer = NewDrawing.AddComponent<LineRenderer>();
@@ -91,16 +85,11 @@ public class Draw : MonoBehaviour
     }
     private void WhileDrawing()
     {
-        //if (HasAnyObjectOnMouse()) { Destroy(NewDrawing); DisposeLineRenderer(); return; }
-        if(Line_Renderer == null) { return; }
-
         Line_Renderer.positionCount++;
         Line_Renderer.SetPosition(Line_Renderer.positionCount - 1, mousePos);
     }
     private void EndDrawing()
     {
-        if(Line_Renderer == null) { return; }
-
         Line_Renderer.Simplify(SimplificationCoefficient);
 
         Mesh LineMesh = new();
@@ -115,6 +104,7 @@ public class Draw : MonoBehaviour
         DisposeLineRenderer();
 
         Rigidbody2D rb = NewDrawing.AddComponent<Rigidbody2D>();
+        rb.sharedMaterial = physicMaterial2D;
         //rb.sleepMode = RigidbodySleepMode2D.NeverSleep;
         rb.useAutoMass = true;
         rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
@@ -137,11 +127,12 @@ public class Draw : MonoBehaviour
 
             for (int i = 0; i < positions.Length - 1; i++)
             {
-                if(isBox)
+                if (isBox)
                 {
                     GameObject box = new ("Box Collider " + i);
                     BoxCollider2D collider = box.AddComponent<BoxCollider2D>();
                     collider.isTrigger = true;
+                    collider.sharedMaterial = physicMaterial2D;
                     box.AddComponent<CollisionChecker>().col = collider;
                     collider.transform.position = (positions[i] + positions[i + 1]) / 2f;
                     float distance = Vector2.Distance(positions[i], positions[i + 1]);
@@ -173,64 +164,9 @@ public class Draw : MonoBehaviour
             Debug.LogException(e);
         }
     }
-    private void RemoveOverlappingColliders(GameObject go)
+    private void GetResources()
     {
-        CapsuleCollider2D[] colliders = go.GetComponentsInChildren<CapsuleCollider2D>();
-
-        foreach (CapsuleCollider2D collider in colliders)
-        {
-            Collider2D[] overlappingColliders = Physics2D.OverlapCapsuleAll(
-                collider.transform.position,
-                collider.size,
-                collider.direction,
-                collider.transform.rotation.eulerAngles.z,
-                collider.gameObject.layer
-            );
-
-            foreach (Collider2D overlappingCollider in overlappingColliders)
-            {
-                if (overlappingCollider != collider)
-                {
-                    Destroy(collider.gameObject);
-                    Debug.Log("Destroyed");
-                    break;
-                }
-            }
-        }
-    }
-    private static void AssignPolygonCollider(LineRenderer lr, GameObject go)
-    {
-        PolygonCollider2D polCol = go.AddComponent<PolygonCollider2D>();
-
-        int posCount = lr.positionCount;
-
-        Vector2[] points = new Vector2[posCount];
-
-        for (int i = 0; i < posCount; i++)
-        {
-            points[i] = (Vector2)lr.GetPosition(i);
-        }
-
-        polCol.points = points;
-
-        Collider2DOptimization.PolygonColliderOptimizer.OptimizePoligonCollider(polCol);
-    }
-    private static void AssignEdgeCollider(LineRenderer lr, GameObject go)
-    {
-        int maxPos = lr.positionCount;
-
-        EdgeCollider2D EdgeCollider = go.AddComponent<EdgeCollider2D>();
-
-        List<Vector2> Points = new();
-
-        for (int i = 0; i < maxPos; i++)
-        {
-            Points.Add((Vector2)lr.GetPosition(i));
-        }
-        EdgeCollider.SetPoints(Points);
-    }
-    private static Material GetLineMaterial()
-    {
-        return Resources.Load(LineMaterialResourcePath) as Material;
+        LineMaterial =  Resources.Load(LineMaterialResourcePath) as Material;
+        physicMaterial2D = Resources.Load(LineMaterialResourcePath) as PhysicsMaterial2D;
     }
 }
--- Assets/Scripts/Drawing/DrawingContainer.cs
+++ Assets/Scripts/Drawing/DrawingContainer.cs
@@ -4,19 +4,24 @@ public class DrawingContainer : MonoBehaviour
     public static DrawingContainer Instance { get; private set; }
     private DrawingContainer() { }
 
+    [Header("Warning Label")]
+    [SerializeField] private bool isDisplaying = false;
+    [SerializeField] private string Gui;
+    [SerializeField] private string CannotSpawnWarning = "You can not spawn a colliding drawn object";
+    [SerializeField] private float DisplayCounter = 2f;
+    private float initialCounter = 0f;
+
     [Header("Child Operations")]
     [SerializeField] private int ChildCount = 0;
     private void Awake()
     {
         CheckInstance();
-    }
-    private void Start()
-    {
-        
+        initialCounter = DisplayCounter;
     }
     private void Update()
     {
-        ChildListener();  
+        ChildListener();
+        DisplayTimer();
     }
     private void CheckInstance()
     {
@@ -38,6 +43,25 @@ public class DrawingContainer : MonoBehaviour
             }
         }
     }
+    private void DisplayTimer()
+    {
+        if (!isDisplaying) { return; }
+        else
+        {
+            DisplayCounter -= Time.deltaTime;
+
+            if(DisplayCounter <= 0f)
+            {
+                isDisplaying = false;
+                DisplayCounter = initialCounter;
+            }
+        }  
+    }
+    private void DisplayError()
+    {
+        if (isDisplaying) { DisplayCounter = initialCounter; }
+        else { isDisplaying = true; }
+    }
     private void ChildListener()
     {
         int currentChildCount = transform.childCount;
@@ -46,13 +70,22 @@ public class DrawingContainer : MonoBehaviour
 
         if (transform.childCount > ChildCount)
         {
-            Debug.Log("Child Added");
+            //Debug.Log("Child Added");
             ChildCount++;
         }
         else // if(transform.childCount < ChildCount)
         {
-            Debug.Log("Child removed");
+            //Debug.Log("Child removed");
             ChildCount--;
+            DisplayError();
         }
+    }
+    private void OnGUI()
+    {
+        if(isDisplaying)
+        {
+            Gui = GUI.TextField(new Rect(Screen.width / 2 - 80, Screen.height / 2, Screen.height/20, Screen.height / 20), CannotSpawnWarning, 100, GUIStyle.none);
+        }
+        
     }
 }
