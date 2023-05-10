f0732a2e048876649ead90f9666a288dbd85ef86
Atahan Ekici <atahanekici@hotmail.com>
2023-05-10 11:16:26 +0300

    Small Additions

--- Assets/Scripts/Ball/BallController.cs
+++ Assets/Scripts/Ball/BallController.cs
@@ -3,6 +3,8 @@ public class BallController : MonoBehaviour
 {
     public const string BallTag = "Ball";
 
+    public const string DestroyParticle_Location = "Particles/BallDestroy";
+
     [Header("Burning Controlls")]
     [SerializeField] private bool isBurning = false;
     [SerializeField] private float BurnSpeed = 2f;
@@ -14,18 +16,27 @@ public class BallController : MonoBehaviour
     [Header("Renderer")]
     [SerializeField] private Renderer BallRenderer;
 
+    [Header("Particles")]
+    [SerializeField] private GameObject DestroyParticle;
+    [SerializeField] private GameObject FlameParticle;
+
     private void Awake()
     {
         GetLocalReferences();
     }
     private void Start()
     {
+        GetResources();
         StartUp();
     }
     private void Update()
     {
         CoolDown();
         FireUp();
+    }
+    private void GetResources()
+    {
+        DestroyParticle = Resources.Load<GameObject>(DestroyParticle_Location) as GameObject;
     }
     private void GetLocalReferences()
     {
@@ -78,11 +89,38 @@ public class BallController : MonoBehaviour
         isBurning = false;
         BurnSpeed = speed;
     }
+    private void SpawnDestroyedBallParticle()
+    {
+        try
+        {
+            GameObject DestructionParticle = Instantiate(DestroyParticle,transform.position,Quaternion.identity);
 
+            ParticleSystem Particle = DestructionParticle.GetComponent<ParticleSystem>();
+            ParticleSystem.MainModule mainModule = Particle.main;
+
+            ParticleSystemRenderer renderer = DestructionParticle.GetComponent<ParticleSystemRenderer>();
+            Material ParticleMaterial = renderer.material;
+            
+
+            float newSize = 0.2f;
+            mainModule.startSize = newSize;
+            Debug.Log(newSize);
+
+            ParticleMaterial.SetColor("_Color", BallRenderer.material.color);
+        }
+        catch(System.Exception e)
+        {
+            Debug.LogException(e);
+        }
+        finally
+        {
+            //Debug.Log("Destruction Particle could be FAILED!");
+        }
+    }
     public void DestroyBall()
     {
         // Game Over  // 
-        // Spawn Destruction Particle //
+        SpawnDestroyedBallParticle();// Spawn Destruction Particle //
         Destroy(gameObject);
     }
 }
